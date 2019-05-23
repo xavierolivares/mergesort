4339f14552b892bad653ce8f1fe896ba24036e64
Xavier Olivares <xm.olivares@gmail.com>
2019-05-23 13:20:26 -0400

    algo work

--- mergesort.js
+++ mergesort.js
@@ -1,12 +1,59 @@
-function split (array) {
-    if (array.length === 1) {
-        return array;
+// function merge (leftArray, rightArray) {
+//     let leftIdx = 0;
+//     let rightIdx = 0;
+//     let sortedArray = [];
+    
+//     while(leftIdx < leftArray.length && rightIdx < rightArray.length) {
+//         if(leftArray[leftIdx] < rightArray[rightIdx]){
+//             sortedArray.push(leftArray[leftIdx]);
+//             leftIdx++;
+//         } else {
+//             sortedArray.push(rightArray[rightIdx]);
+//             rightIdx++;
+//         }
+//     }
+
+//     return sortedArray.concat(rightArray.slice(rightIdx)).concat(leftArray.slice(leftIdx))
+// }
+function merge (leftArray, rightArray) {
+    let leftIdx = 0;
+    let rightIdx = 0;
+    let sortedArray = [];
+    
+    while(leftIdx < leftArray.length && rightIdx < rightArray.length) {
+        if(leftArray[leftIdx] < rightArray[rightIdx]){
+            sortedArray.push(leftArray[leftIdx]);
+            leftIdx++;
+        } else {
+            sortedArray.push(rightArray[rightIdx]);
+            rightIdx++;
+        }
     }
 
-    let middlePoint = Math.floor(array.length / 2)
-    console.log('this is the midpoint', middlePoint)
+    return sortedArray
+        .concat(rightArray.slice(rightIdx))
+        .concat(leftArray.slice(leftIdx))
+}
 
+function mergeSort (array) {
+    if (array.length <= 1) {
+        return array;
+    } 
+    let middlePoint = Math.floor(array.length / 2)
     let leftArray = array.slice(0,middlePoint)
     let rightArray = array.slice(middlePoint)
-    return [leftArray, rightArray]
+
+    return merge(mergeSort(leftArray), mergeSort(rightArray))
 }
+
+
+// function mergeSort(array) {
+//     merge(split(array))
+// }
+
+
+// split is the base case
+
+// if array is one element, it's sorted
+// otherwise, split the array and merge sort each half
+// merge combined halves into sorted whole
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,12 +1,15 @@
 
 describe('Split Array function', function() {
-    it('is able to split array into two halves', function(){
-        expect(split([1,2])).toEqual([[1],[2]]);
-    })
-    it('returns an array of a single element for input array of one element', function() {
-        expect(split([1])).toEqual([1]);
-    })
-    it('correctly splits uneven array lengths', function(){
-        expect(split([1,2,3])).toEqual([[1],[2,3]])
+    // it('is able to split array into two halves', function(){
+    //     expect(split([1,2])).toEqual([[1],[2]]);
+    // })
+    // it('returns an array of a single element for input array of one element', function() {
+    //     expect(split([1])).toEqual([[1]]);
+    // })
+    // it('correctly splits uneven array lengths', function(){
+    //     expect(split([1,2,3,4,5,6])).toEqual([[1],[2],[3],[4],[5],[6]])
+    // })
+    it('puts in dat WORK', function() {
+        expect(mergeSort([5,2,3,4,1,6])).toEqual([1,2,3,4,5,6])
     })
 })
